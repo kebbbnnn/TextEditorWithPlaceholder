8e67d07487eae2760b497e1f7e41adbf7f80e419
Kevin Ladan <kevinladan@outlook.com>
2024-10-23 21:02:20 +0800

    the code

--- Sources/TextEditorWithPlaceholder/TextEditorWithPlaceholder.swift
+++ Sources/TextEditorWithPlaceholder/TextEditorWithPlaceholder.swift
@@ -1,2 +1,73 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  TextEditorWithPlaceholder.swift
+//  TextEditorWithPlaceholder
+//
+//  Created by 平岡修 on 2022/06/01.
+//
+
+import SwiftUI
+
+public struct TextEditorWithPlaceholder: View {
+    
+    @FocusState private var focusedField: Field?
+    
+    enum Field {
+        case textEditor
+        case placeholder
+    }
+    
+    @Binding var text: String
+    private let placeholderText: String
+    
+    public init(_ placeholder: String, text: Binding<String>) {
+        self._text = text
+        self.placeholderText = placeholder
+    }
+    
+    public var body: some View {
+        ZStack {
+            
+            // Show the placeholder if the text is empty
+            if text.isEmpty {
+                
+                ZStack {
+                    Rectangle()
+                        .fill(Color(uiColor: .systemBackground))
+                        .onTapGesture {
+                            focusedField = .placeholder
+                        }
+                    
+                    VStack {
+                        HStack {
+                            TextField(placeholderText, text: $text)
+                                .focused($focusedField, equals: .placeholder)
+                                .onAppear {
+                                    focusedField = .placeholder
+                                }
+                                
+                            Spacer()
+                        }
+                        .padding(.leading, 6)
+                        .padding(.top, 8)
+                        
+                        Spacer()
+                    }
+                }
+
+                // Show the text editor if the text is not empty
+            } else {
+                TextEditor(text: $text)
+                    .focused($focusedField, equals: .textEditor)
+                    .onAppear {
+                        focusedField = .textEditor
+                    }
+            }
+        }
+    }
+}
+
+struct TextEditorWithPlaceholder_Previews: PreviewProvider {
+    static var previews: some View {
+        TextEditorWithPlaceholder("Write down...", text: .constant(""))
+    }
+}
