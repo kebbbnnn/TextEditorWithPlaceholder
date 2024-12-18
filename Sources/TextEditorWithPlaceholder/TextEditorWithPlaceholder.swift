f62938361c4bf38362d092ca290abc4b3011bc41
Kevin Ladan <kevinladan@outlook.com>
2024-12-18 11:10:50 +0800

    fixed some bugs

--- Sources/TextEditorWithPlaceholder/TextEditorWithPlaceholder.swift
+++ Sources/TextEditorWithPlaceholder/TextEditorWithPlaceholder.swift
@@ -18,51 +18,57 @@ public struct TextEditorWithPlaceholder: View {
     
     @Binding var text: String
     private let placeholderText: String
+    private let shouldAutoFocus: Bool
     
-    public init(_ placeholder: String, text: Binding<String>) {
+    public init(_ placeholder: String, text: Binding<String>, shouldAutoFocus: Bool = false) {
         self._text = text
         self.placeholderText = placeholder
+        self.shouldAutoFocus = shouldAutoFocus
     }
     
     public var body: some View {
         ZStack {
             
             // Show the placeholder if the text is empty
-            if text.isEmpty {
+            ZStack {
+                Rectangle()
+                    .fill(Color.clear)
+                    .onTapGesture {
+                        guard self.shouldAutoFocus else { return }
+                        focusedField = .placeholder
+                    }
                 
-                ZStack {
-                    Rectangle()
-                        .fill(Color(uiColor: .systemBackground))
-                        .onTapGesture {
-                            focusedField = .placeholder
-                        }
-                    
-                    VStack {
-                        HStack {
-                            TextField(placeholderText, text: $text)
-                                .focused($focusedField, equals: .placeholder)
-                                .onAppear {
-                                    focusedField = .placeholder
-                                }
-                                
-                            Spacer()
-                        }
-                        .padding(.leading, 6)
-                        .padding(.top, 8)
+                VStack {
+                    HStack {
+                        TextField(placeholderText, text: $text)
+                            .focused($focusedField, equals: .placeholder)
+                            .onAppear {
+                                guard self.shouldAutoFocus else { return }
+                                focusedField = .placeholder
+                            }
                         
                         Spacer()
                     }
+                    .padding(.leading, 6)
+                    .padding(.top, 8)
+                    
+                    Spacer()
                 }
-
-                // Show the text editor if the text is not empty
-            } else {
-                TextEditor(text: $text)
-                    .focused($focusedField, equals: .textEditor)
-                    .onAppear {
-                        focusedField = .textEditor
-                    }
             }
+            .opacity(text.isEmpty ? 1 : 0)
+            
+            // Show the text editor if the text is not empty
+            TextEditor(text: $text)
+                .scrollContentBackground(.hidden)
+                .background(Color.clear)
+                .focused($focusedField, equals: .textEditor)
+                .onAppear {
+                    guard self.shouldAutoFocus else { return }
+                    focusedField = .textEditor
+                }
+                .opacity(text.isEmpty ? 0 : 1)
         }
+        .padding(.leading, -6)
     }
 }
 
